abc605402e51e94829fd0bd63617cec8e10674e7
guerreroandres <guerreroandres@gmail.com>
2021-06-30 06:17:45 -0500

    changed dockerfile to include timeoff-management

--- aws/ci.tf
+++ aws/ci.tf
@@ -49,53 +49,6 @@ resource "aws_iam_user_policy" "publisher" {
 EOF
 }
 
-resource "aws_iam_role" "fargate" {
-  name = "fargate-role"
-  path = "/serviceaccounts/"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = [
-            "ecs.amazonaws.com",
-            "ecs-tasks.amazonaws.com"
-          ]
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "fargate" {
-  name = "fargate-execution-role"
-  role = aws_iam_role.fargate.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ecr:CompleteLayerUpload",
-        "ecr:DescribeRepositories",
-        "ecr:ListImages",
-        "ecr:DescribeImages",
-        "ecr:GetAuthorizationToken",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:GetLifecyclePolicy"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 output "publisher_access_key" {
   value       = aws_iam_access_key.publisher.id
   description = "AWS_ACCESS_KEY to publish to ECR"
