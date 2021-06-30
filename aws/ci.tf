2b3aaf0e857729215cc252fcf37764bdf7bb31ad
guerreroandres <guerreroandres@gmail.com>
2021-06-29 19:05:28 -0500

    updated publisher role

--- aws/ci.tf
+++ aws/ci.tf
@@ -7,6 +7,41 @@ resource "aws_iam_access_key" "publisher" {
   user = aws_iam_user.publisher.name
 }
 
+resource "aws_iam_user_policy" "publisher" {
+  name = "ecr-publisher"
+  user = aws_iam_user.publisher.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iam:PassRole",
+        "iam:GetRole",
+        "ecs:DescribeTaskDefinition",
+        "ecs:DescribeServices",
+        "ecs:UpdateService",
+        "ecs:RegisterTaskDefinition",
+        "ecr:CompleteLayerUpload",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetLifecyclePolicy",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 output "publisher_access_key" {
   value       = aws_iam_access_key.publisher.id
   description = "AWS_ACCESS_KEY to publish to ECR"
